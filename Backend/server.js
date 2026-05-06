ad06aed284ca4cbccb3940f8af954a3ddd65231f
Roshan-trial <iambackganesha@gmail.com>
2026-05-06 16:25:33 +0530

    cors allowed

--- Backend/server.js
+++ Backend/server.js
@@ -44,7 +44,7 @@ app.use(session({
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://e-commerce-p0de.onrender.com/"],
     credentials: true
 }));
 
