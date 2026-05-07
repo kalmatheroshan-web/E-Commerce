3635bbb530f85b14797f0dfc12d2eda29e183ade
kalmatheroshan-web <kalmatheroshan@gmail.com>
2026-05-07 11:11:17 +0530

    Update sendmail.js

--- Backend/Services/sendmail.js
+++ Backend/Services/sendmail.js
@@ -7,6 +7,9 @@ const transporter = nodemailer.createTransport({
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD,
     },
+    connectionTimeout: 10000, 
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
 
 async function sendMail(email, subject, text, html) {
