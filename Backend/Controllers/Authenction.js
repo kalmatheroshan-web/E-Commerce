32d48f9466977fc3490ff97680bebd77b1e1b1dc
kalmatheroshan-web <kalmatheroshan@gmail.com>
2026-05-07 10:15:50 +0530

    some removal did

--- Backend/Controllers/Authenction.js
+++ Backend/Controllers/Authenction.js
@@ -116,19 +116,19 @@ async function login(req, res) {
         const otp = Math.floor(100000 + Math.random() * 900000);
 
         // 4. Update or Create OTP record
-        await otpModel.findOneAndUpdate(
-            { email },
-            { otp, createdAt: new Date() },
-            { upsert: true, new: true }
-        );
+        // await otpModel.findOneAndUpdate(
+        //     { email },
+        //     { otp, createdAt: new Date() },
+        //     { upsert: true, new: true }
+        // );
 
         // 5. Send Email
-        await sendMail(
-            email,
-            "OTP Verification !!",
-            `Your OTP is ${otp}`,
-            otp_html(otp)
-        );
+        // await sendMail(
+        //     email,
+        //     "OTP Verification !!",
+        //     `Your OTP is ${otp}`,
+        //     otp_html(otp)
+        // );
 
         return res.status(200).json({
             mes: "OTP sent to your email",
