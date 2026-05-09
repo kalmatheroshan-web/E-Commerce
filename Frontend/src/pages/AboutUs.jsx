434b8136c6dbb9d96d2a194774449ef50b2d4f61
Roshan-trial <iambackganesha@gmail.com>
2026-05-09 13:03:02 +0530

    AboutUs changes

--- Frontend/src/pages/AboutUs.jsx
+++ Frontend/src/pages/AboutUs.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function AboutUs() {
     return (
-        <div className="bg-white mt-20 text-gray-800">
+        <div className="bg-white text-gray-800">
 
             <section className="bg-indigo-600 text-white py-16 px-6 text-center">
                 <h1 className="text-3xl md:text-5xl font-bold mb-4">
